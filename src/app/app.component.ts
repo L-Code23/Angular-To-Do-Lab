caf209ce58a58ace8761ce29194967167be0afdd
Ramsey Louder <RamseyLouder@gmail.com>
2024-06-13 23:44:53 -0400

    finished To-Do Lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToDo } from './models/to-do';
+import { FormsModule } from '@angular/forms';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule,DecimalPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngToDoLab';
+  title: string = 'AngToDoLab';
+
+  formToDo: ToDo = {} as ToDo;
+  allToDos:ToDo [] = [
+    {
+      task: "Cut Grass",
+      completed: true,
+      duration: .5,
+      priority: "high"
+
+  },
+  {
+      task: "dishes",
+      completed: false,
+      duration: .25,
+      priority: "high"
+  },
+  {
+      task: "vaccuum",
+      completed: false,
+      duration: .25,
+      priority: "medium"
+  },
+  ]
+  completeTask(targetToDo:ToDo):void{
+    targetToDo.completed = true;
+  }
+  deleteTask(targetToDo:ToDo):void{
+    let index:number = this.allToDos.findIndex(p=> p == targetToDo);
+    this.allToDos.splice(index, 1)
+}
+
+AddTask():void{
+  let newToDo:ToDo = {...this.formToDo};
+      this.formToDo.completed= false;
+      this.allToDos.push(newToDo);
+      console.log(this.allToDos);
+}
+
+
+
+
 }
